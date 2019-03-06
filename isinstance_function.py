8f4cd533e9225e795cbf99444c2d6cf60196713c
unknown <noolivaralakshmi@gmail.com>
2019-03-06 23:52:50 +0530

    instancefunction

---
+++ isinstance_function.py
@@ -0,0 +1,9 @@
+num = 100
+# true because num is an integer
+print(isinstance(num, int))
+
+# false because num is not a float
+print(isinstance(num, float))
+
+# false because num is not a complex number
+print(isinstance(num, complex))
