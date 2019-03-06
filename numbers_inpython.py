9c23c3b25464b3dfbf9d323231a12de304925d89
unknown <noolivaralakshmi@gmail.com>
2019-03-06 23:49:13 +0530

    addingnumbers

---
+++ numbers_inpython.py
@@ -0,0 +1,17 @@
+# Python program to display numbers of
+# different data types
+
+# int
+num1 = 10
+num2 = 100
+print(num1+num2)
+
+# float
+a = 10.5
+b = 8.9
+print(a-b)
+
+# complex numbers
+x = 3 + 4j
+y = 9 + 8j
+print(y-x)
