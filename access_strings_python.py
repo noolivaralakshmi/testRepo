0af522c88cbf6cf5ca444494d29007675b37cadb
unknown <noolivaralakshmi@gmail.com>
2019-03-07 00:20:20 +0530

    accessstrings

---
+++ access_strings_python.py
@@ -0,0 +1,16 @@
+str = "Kevin"
+
+# displaying whole string
+print(str)
+
+# displaying first character of string
+print(str[0])
+
+# displaying third character of string
+print(str[2])
+
+# displaying the last character of the string
+print(str[-1])
+
+# displaying the second last char of string
+print(str[-2])
