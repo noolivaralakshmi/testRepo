2700320c5a9379016dfb2802e1a0b49f3df68feb
unknown <noolivaralakshmi@gmail.com>
2019-03-07 00:13:50 +0530

    stringsinpython

---
+++ strings_python.py
@@ -0,0 +1,15 @@
+# lets see the ways to create strings in Python
+str = 'beginnersbook'
+print(str)
+
+str2 = "Chaitanya"
+print(str2)
+
+# multi-line string
+str3 = """Welcome to 
+   Beginnersbook.com"""
+print(str3)
+
+str4 = '''This is a tech 
+   blog'''
+print(str4)
