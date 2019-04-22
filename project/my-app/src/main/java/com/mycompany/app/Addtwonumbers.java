976ce70782770f521b07ebd391019d3d5a76796b
noolivaralakshmi <noolivaralakshmi@gmail.com>
2019-04-22 16:48:30 +0530

    addtwo numbers

---
+++ project/my-app/src/main/java/com/mycompany/app/Addtwonumbers.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+public class Addtwonumbers {
+	public static void main(String[] args) {
+        
+	      int num1 = 5, num2 = 15, sum;
+	      sum = num1 + num2;
+
+	      System.out.println("Sum of these numbers: "+sum);
+	   }
+}
+
+
